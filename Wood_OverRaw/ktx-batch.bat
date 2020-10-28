2d67ed33ae81152987fe973df67524d6e6f2dbe6
papatel1612 <papatel@hntb.com>
2020-10-28 16:20:08 -0500

    Add files via upload

---
+++ Wood_OverRaw/ktx-batch.bat
@@ -0,0 +1,4 @@
+call ktx-files.bat Wood_basecolor.jpg Wood_basecolor 'N' '' 
+call ktx-files.bat Wood_M_R.jpg Wood_M_R 'N' '' 
+call ktx-files.bat Wood_normal.jpg Wood_normal 'N' '' 
+call ktx-files.bat Wood_roughness.jpg Wood_roughness 'N' '' 
